630087eda768047e9851e9f2a58de5a6b0f841b5
tushala <1941603841@qq.com>
2020-01-12 12:30:22 +0800

    添加注释一行

--- 高级编程/ch5自定义序列类/array_test.py
+++ 高级编程/ch5自定义序列类/array_test.py
@@ -5,3 +5,6 @@ import array
 my_array = array.array("i")  # 存放为int数据类型
 my_array.append(1)
 my_array.append("abc")  # TypeError: an integer is required (got type str)
+
+
+# 在list中的数据类型保存的是数据的存放的地址，简单的说就是指针，并非数据
