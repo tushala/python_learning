4aa8b5a52f8e847c011259d73669ca60f6648ba3
tushala <1941603841@qq.com>
2020-01-15 23:48:46 +0800

    完善ch6

---
+++ 高级编程/ch6set&dict/set_test.py
@@ -0,0 +1,14 @@
+# set 操作
+
+s = {"a", "b"}
+# s.add("c")  # add
+# s.update(set("plm"))  # update
+
+# another_set = s.difference(set("bc"))  # difference
+# print(another_set)
+
+# - & |
+print(s.issubset(set("abc")))
+
+
+print(s)
